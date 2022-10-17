dd434e9edf29c9533e69f107680904181a3edf34
Mark Valentine Aikins <valentineaikins@icloud.com>
2022-10-17 12:30:04 +0000

    first Python program

---
+++ InputApp.py
@@ -0,0 +1,8 @@
+#firstName = input("Enter the name of the favourite pet: ")
+#print("Your favourite pet name is: " + firstName)
+#checkType = input("What is the string that will be displayed inputApp")
+#print(type(checkType))
+first_Name = input("What is your first name ")
+last_Name = input("What is your last name ")
+full_Name = first_Name + " " + last_Name
+print("Your full name is " + full_Name)
